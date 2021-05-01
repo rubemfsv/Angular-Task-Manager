6ed2f7a8e52b7559046b5a2eb70addd951031924
Rubem <rfsv@ic.ufal.br>
2021-05-01 15:41:59 -0300

    feature: create tasks module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { TasksModule } from './tasks';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, TasksModule],
   providers: [],
   bootstrap: [AppComponent],
 })
