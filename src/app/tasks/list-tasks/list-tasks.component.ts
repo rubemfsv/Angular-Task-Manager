5c2392d4a7acb852c0e8774cef7fcf0d31f75105
Rubem <rfsv@ic.ufal.br>
2021-05-03 21:15:12 -0300

    feature: tasks list implementation

--- src/app/tasks/list-tasks/list-tasks.component.ts
+++ src/app/tasks/list-tasks/list-tasks.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Task, TaskService } from '../shared';
 
 @Component({
   selector: 'app-list-tasks',
   templateUrl: './list-tasks.component.html',
-  styleUrls: ['./list-tasks.component.css']
+  styleUrls: ['./list-tasks.component.css'],
 })
 export class ListTasksComponent implements OnInit {
+  tasks: Task[];
 
-  constructor() { }
+  constructor(private taskService: TaskService) {}
 
   ngOnInit(): void {
+    this.tasks = this.listAllTasks();
   }
 
+  listAllTasks(): Task[] {
+    return this.taskService.listAllTasks();
+  }
+
+  remove($event: any, task: Task): void {
+    $event.preventDefault();
+
+    if (confirm('Do you want to remove')) {
+    }
+  }
 }
--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 import { TaskService } from './shared';
 import { ListTasksComponent } from './list-tasks';
 
 @NgModule({
   declarations: [ListTasksComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   providers: [TaskService],
 })
 export class TasksModule {}
