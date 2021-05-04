0c6eef1f520d2b4a06edde66998db43fb7643dde
Rubem <rfsv@ic.ufal.br>
2021-05-03 22:52:23 -0300

    feature: create finished task directive

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
-import { TaskService } from './shared';
+import { TaskService, FinishedTaskDirective } from './shared';
 import {
   ListTasksComponent,
   RegisterTaskComponent,
@@ -11,7 +11,12 @@ import {
 } from './';
 
 @NgModule({
-  declarations: [ListTasksComponent, RegisterTaskComponent, EditTaskComponent],
+  declarations: [
+    ListTasksComponent,
+    RegisterTaskComponent,
+    EditTaskComponent,
+    FinishedTaskDirective,
+  ],
   imports: [CommonModule, RouterModule, FormsModule],
   providers: [TaskService],
 })
